9505aec74022c073627ad72323f45de1594c9db5
ROLAND OBIORA <0xfortunatepanda@proton.me>
2026-05-18 16:47:47 +0100

    pipleine 7

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "s3-pod1-acebucket
-"
-    key    = "s3-pod1-acebucket
-/prodution/terraform.tfstate"
+    bucket = "s3-pod1-acebucket"
+    key    = "s3-pod1-acebucket/prodution/terraform.tfstate"
     region = "us-west-1"
   }
 }
