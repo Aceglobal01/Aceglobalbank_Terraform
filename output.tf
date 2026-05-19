c395a8ead531b2b2800fe9ef08e8f09364672b43
ROLAND OBIORA <0xfortunatepanda@proton.me>
2026-05-19 23:01:20 +0100

    pipleine 25

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "nginx_lb_dns" {
   description = "The DNS name of the NGINX Ingress Load Balancer"
-  value       = try(data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].hostname, null)
+  value       = module.eks-deployment.nginx_lb_dns 
 }
