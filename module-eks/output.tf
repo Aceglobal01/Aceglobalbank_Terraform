495b756874e0deafe260b7c9093e4e2207b71b36
ROLAND OBIORA <0xfortunatepanda@proton.me>
2026-05-19 22:55:58 +0100

    pipleine 23

--- module-eks/output.tf
+++ module-eks/output.tf
@@ -1,10 +1,4 @@
-output "nginx_ingress_lb_dns" {
-  value = data.aws_lb.nginx_ingress.dns_name
-}
-
-output "nginx_lb_ip" {
-  value = data.aws_lb.nginx_ingress.ip_address_type == "ipv4" ? data.aws_lb.nginx_ingress.dns_name : ""
-}
-output "nginx_ingress_load_balancer_hostname" {
-  value = data.aws_lb.nginx_ingress.dns_name
+output "nginx_lb_dns" {
+  description = "The DNS name of the NGINX Ingress Load Balancer"
+  value       = try(data.kubernetes_service.ingress_nginx.status[0].load_balancer[0].ingress[0].hostname, null)
 }
